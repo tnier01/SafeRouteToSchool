23b22ccc6f0a09c05fd3bf5d3c2533e3b4246624
NJaku01 <nick1212@gmx.de>
2020-12-21 11:20:26 +0100

    node server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use("/js", express.static(__dirname + '/js'))
+app.use("/leaflet", express.static(__dirname + "/node_modules/leaflet/dist"));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+
+app.use("/data", express.static(__dirname + '/data'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Map.html')
+})
+
+const port = 3000;
+
+app.listen(port,
+    () => console.log(`Example app listening at http://localhost:${port}`)
+)
