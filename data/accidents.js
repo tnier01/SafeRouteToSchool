1054d8d8a213e45a478432b3ed593e4c47bdf762
jan <j_sten07@uni-muenster.de>
2020-12-27 16:16:19 +0100

    accidents: geojson and different colors

--- data/accidents.js
+++ data/accidents.js
@@ -1,4 +1,4 @@
-{
+accidents = {
 "type": "FeatureCollection",
 "name": "accidents",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
---
+++ js/accidents.js
@@ -0,0 +1,52 @@
+function getColor(p) {
+    return p.IstRad == 1 ? '#ff1200':
+        '#ff7800';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties),
+        radius: 8,
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+}
+
+function popup(feature, layer) {
+    layer.bindPopup("time: " + feature.properties.USTUNDE + " h")
+}
+
+L.geoJSON(accidents, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, style(feature));
+    },
+    onEachFeature: popup
+}).addTo(mymap);
+
+
+var legend = L.control({position: 'bottomright'});
+
+/*
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    labels = ['<strong>Involved</strong>'],
+        categories = ['Bicycle', 'Other'];
+        colors = ['#ff1200', '#ff7800'];
+
+    for (var i = 0; i < categories.length; i++) {
+
+        div.innerHTML +=
+            labels.push(
+                '<i class="circle" style="fillcolor:' + colors[i] + '"></i> ' +
+                (categories[i] ? categories[i] : '+'));
+
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(mymap);
+*/
